3dffe2a79f92b09700c4a749df21d2e0ed58e5ed
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-26 21:48:06 +0200

    Add project files.

---
+++ Uppgift frång CProv-Jesper Wintberg/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    Console.Write("Hur många numer vill du skriva in: ");
+                    int mängd = int.Parse(Console.ReadLine());
+                    int högsta = 0, lägsta = Int32.MaxValue, summa = 0;
+
+                    for (int i = 1; i <= mängd; i++)
+                    {
+                        Console.Write($"Tal {i}: ");
+
+                        int nummer = int.Parse(Console.ReadLine());
+                        if (nummer > högsta)
+                        {
+                            högsta = nummer;
+                        }
+                        else if (nummer < lägsta)
+                        {
+                            lägsta = nummer;
+                        }
+
+                        summa += nummer;
+                    }
+
+                    Console.WriteLine($"Högsta talet: {högsta}");
+                    Console.WriteLine($"Lägsta talet: {lägsta}");
+                    Console.WriteLine($"Genomsnitt: {summa / mängd}");
+                }
+            }
+        }
+    }
+}
+
